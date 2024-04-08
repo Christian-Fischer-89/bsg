eca0bc983a7dd00323cebdf02b825414a90062d2
Fischer <christian.fischer@thalia.de>
2024-04-08 23:57:59 +0200

    added news content

--- script_2.js
+++ script_2.js
@@ -1,5 +1,7 @@
+//navigation styling
+
 function check(clsName, me) {
-    elements = document.getElementsByClassName(clsName);
+    const elements = document.getElementsByClassName(clsName);
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.backgroundColor="rgb(0, 63, 31)";
         elements[i].style.color="white";
@@ -9,6 +11,8 @@ function check(clsName, me) {
     document.getElementById(me).style.color = "black";
     document.getElementById(me).style.scale = "1.25";
 }
+
+//mobile navigation
 
 const mobNav = document.querySelector(".mob-nav");
 const mobNavbar = document.querySelector(".mob-navbar");
@@ -24,6 +28,8 @@ mobNav.addEventListener("click", () => {
     }
 });
 
+//image upload
+
 function loadImg(path) {
     var gallery = document.querySelectorAll(".img-grid");
     gallery.forEach((div) => {
@@ -36,25 +42,21 @@ function loadImg(path) {
     });
 }
 
-function loadImg(path) {
-    var gallery = document.querySelectorAll(".img-grid");
-    gallery.forEach((div) => {
-        for (var i = 1; i <= div.dataset.count; i++) {
-        const img = document.createElement("img");
-        img.src = "images/" + path + i + ".jpg";
-        img.alt = path + " " + i;
-        img.style.width = "100%";
-        div.appendChild(img);
-        }
-    });
-}
+document.getElementById("gallery").addeventlistener("load", loadImg("test/"));
+
+//Nachrichtenarchiv
 
 function showNews() {
     document.getElementById("news").style.display= "flex";
 };
 
-function dismiss(element) {
-    element.parentNode.style.display= "none";
-};
+//hide overlay
 
-document.getElementById("gallery").addeventlistener("load", loadImg("test/"));
+function hideNews() {
+    document.getElementById("news").style.display= "none";
+    const elements = document.getElementsByTagName("video");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].pause();
+        elements[i].currentTime = 0;
+    }
+};
